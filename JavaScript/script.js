e6b5c88dfb9056c169bddddcb8b792ba575bfcd9
rahul07tiwari <rahultiwari16031@gmail.com>
2024-03-18 04:21:54 +0530

    front-end done almost

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,3 +1,4 @@
+  // crousel........swiper
   var swiper = new Swiper(".swiper-container", {
     spaceBetween: 30,
     effect: "fade",
@@ -7,3 +8,80 @@
         disableOnInteraction: false,
       },
   });
+//testimonials......... swiper
+  var swiper = new Swiper(".swiper-testimonials", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    slidesperview: "3",
+    loop: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 40,
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      640: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    }
+  });
+
+  // inclusion
+
+document.addEventListener("DOMContentLoaded",
+
+function() {
+  fetch("Links.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("Links").innerHTML = data;
+      });
+  fetch("header.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("header").innerHTML = data;
+      });
+
+  fetch("footer.html")
+      .then(response => response.text())
+      .then(data => {
+          document.getElementById("footer").innerHTML = data;
+      });
+});
